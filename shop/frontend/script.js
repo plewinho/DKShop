f2137434e589b5b01d57b2e5bed4a740d487879a
plewinho <plewinho@gmail.com>
2024-05-23 23:17:23 +0200

    banner#1

--- shop/frontend/script.js
+++ shop/frontend/script.js
@@ -1,32 +1,3 @@
-// document.addEventListener("DOMContentLoaded", requestCategories);
-// function requestCategories() {
-//   fetch("http://localhost:8081/shop/backend/menu.php", {
-//     method: "GET",
-//   })
-//     .then((res) => res.json())
-//     .then((data) => {
-//       const nav = document.querySelector(".navigation");
-//       if (data.categories) {
-//         const ul = document.createElement("ul");
-//         data.categories.forEach((cat) => {
-//           const li = document.createElement("li");
-//           li.className = cat;
-//           li.textContent = cat;
-//           li.addEventListener("click", getCategoryProducts);
-//           ul.appendChild(li);
-//         });
-//         nav.append(ul);
-//       }
-//     })
-//     .catch((error) => console.log(error));
-// }
-
-// function getCategoryProducts() {
-//   console.log("object");
-// }
-// a
-
-// Rozpocznij ładowanie danych od razu, nie czekając na załadowanie strony
 const dataPromise = fetch("http://localhost:8081/shop/backend/menu.php", {
   method: "GET",
 })
@@ -34,7 +5,6 @@ const dataPromise = fetch("http://localhost:8081/shop/backend/menu.php", {
   .catch((error) => console.log(error));
 
 document.addEventListener("DOMContentLoaded", () => {
-  // Kiedy strona jest załadowana, dane są już prawdopodobnie dostępne
   dataPromise.then((data) => {
     const nav = document.querySelector(".navigation");
     if (data.categories) {
@@ -52,5 +22,5 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function getCategoryProducts() {
-  console.log("object");
+  console.log("huj");
 }
